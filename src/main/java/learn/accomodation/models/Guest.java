8baf2bbb3dc0eee3bbfaf5762692fb224a429b21
Amal Thomas <amalthomas1510@gmail.com>
2025-06-07 00:33:52 +0530

    Added the Guest model

--- src/main/java/learn/accomodation/models/Guest.java
+++ src/main/java/learn/accomodation/models/Guest.java
@@ -1,4 +1,72 @@
 package learn.accomodation.models;
 
+import java.util.Objects;
+
 public class Guest {
+    private int guestId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private String state;
+
+    public int getGuestId() {
+        return guestId;
+    }
+
+    public void setGuestId(int guestId) {
+        this.guestId = guestId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Guest guest = (Guest) o;
+        return guestId == guest.guestId && Objects.equals(firstName, guest.firstName) && Objects.equals(lastName, guest.lastName) && Objects.equals(email, guest.email) && Objects.equals(phone, guest.phone) && Objects.equals(state, guest.state);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(guestId, firstName, lastName, email, phone, state);
+    }
 }
