4bad75aedfce524068d126a90261435b137fde7e
Amal Thomas <amalthomas1510@gmail.com>
2025-06-07 00:41:28 +0530

    Added DataException

--- src/main/java/learn/accomodation/data/DataException.java
+++ src/main/java/learn/accomodation/data/DataException.java
@@ -1,7 +1,16 @@
 package learn.accomodation.data;
 
-public class DataException extends RuntimeException {
+public class DataException extends Exception {
+
     public DataException(String message) {
         super(message);
+    }
+
+    public DataException(Throwable cause) {
+        super(cause);
+    }
+
+    public DataException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
