8be239e730a21aba3a34ad94294788382f6444b5
Amal Thomas <amalthomas1510@gmail.com>
2025-06-07 00:39:29 +0530

    Added the Reservation model

--- src/main/java/learn/accomodation/models/Reservation.java
+++ src/main/java/learn/accomodation/models/Reservation.java
@@ -1,4 +1,74 @@
 package learn.accomodation.models;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Objects;
+
 public class Reservation {
+    private Host host;
+    private String hostId;
+    private Guest guest;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private BigDecimal total;
+
+    public Host getHost() {
+        return host;
+    }
+
+    public void setHost(Host host) {
+        this.host = host;
+    }
+
+    public String getHostId() {
+        return hostId;
+    }
+
+    public void setHostId(String hostId) {
+        this.hostId = hostId;
+    }
+
+    public Guest getGuest() {
+        return guest;
+    }
+
+    public void setGuest(Guest guest) {
+        this.guest = guest;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    public void setTotal(BigDecimal total) {
+        this.total = total;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Reservation that = (Reservation) o;
+        return Objects.equals(host, that.host) && Objects.equals(hostId, that.hostId) && Objects.equals(guest, that.guest) && Objects.equals(startDate, that.startDate) && Objects.equals(endDate, that.endDate) && Objects.equals(total, that.total);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(host, hostId, guest, startDate, endDate, total);
+    }
 }
