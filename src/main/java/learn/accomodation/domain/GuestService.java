4a5c6025d9593eb1c7e35dc7ac0a5cafa322fb1f
Amal Thomas <amalthomas1510@gmail.com>
2025-06-06 18:45:46 +0530

    Added required methods to GuestService

--- src/main/java/learn/accomodation/domain/GuestService.java
+++ src/main/java/learn/accomodation/domain/GuestService.java
@@ -1,4 +1,16 @@
 package learn.accomodation.domain;
 
+import learn.accomodation.data.GuestRepository;
+import learn.accomodation.models.Host;
+
 public class GuestService {
+    private GuestRepository repository;
+
+    public GuestService(GuestRepository repository) {
+        this.repository = repository;
+    }
+
+    public Host findByEmail(String email) {
+        return repository.findGuestByEmail(email);
+    }
 }
