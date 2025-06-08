0cfcb83b87d6b592e1a14abf4345f4061b6e6fdd
Amal Thomas <amalthomas1510@gmail.com>
2025-06-08 16:08:07 +0530

    Completed update method in ReservationFileRepository

--- src/main/java/learn/accomodation/data/ReservationFileRepository.java
+++ src/main/java/learn/accomodation/data/ReservationFileRepository.java
@@ -57,8 +57,16 @@ public class ReservationFileRepository implements ReservationRepository{
         return reservation;
     }
 
-    public boolean update(Reservation reservation) {
-
+    public boolean update(Reservation reservation) throws DataException{
+        List<Reservation> all = findForHost(reservation.getHost());
+        for (int i = 0; i < all.size(); i++) {
+            if (all.get(i).getId() == reservation.getId()) {
+                all.set(i, reservation);
+                writeAll(all, reservation.getHost().getHostId());
+                return true;
+            }
+        }
+        return false;
     }
 
     public boolean delete(Reservation reservation) throws DataException {
