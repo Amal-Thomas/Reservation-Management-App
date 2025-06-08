a96f5e660b92c8de8776dfa348c56c541ff3c639
Amal Thomas <amalthomas1510@gmail.com>
2025-06-08 21:26:35 +0530

    Updated validation rules in ReservationService

--- src/main/java/learn/accomodation/domain/ReservationService.java
+++ src/main/java/learn/accomodation/domain/ReservationService.java
@@ -1,5 +1,8 @@
 package learn.accomodation.domain;
 
+import learn.accomodation.data.DataException;
+import learn.accomodation.data.GuestRepository;
+import learn.accomodation.data.HostRepository;
 import learn.accomodation.data.ReservationRepository;
 import learn.accomodation.models.Guest;
 import learn.accomodation.models.Host;
@@ -10,127 +13,204 @@ import java.util.List;
 
 public class ReservationService {
 
-    private ReservationRepository repository;
+    private final GuestRepository guestRepository;
+    private final HostRepository hostRepository;
+    private final ReservationRepository reservationRepository;
 
-    public ReservationService(ReservationRepository repository) {
-        this.repository = repository;
+    public ReservationService(GuestRepository guestRepository, HostRepository hostRepository, ReservationRepository reservationRepository) {
+        this.guestRepository = guestRepository;
+        this.hostRepository = hostRepository;
+        this.reservationRepository = reservationRepository;
     }
 
     public List<Reservation> findForHost(Host host) {
-        return repository.findForHost(host);
+        return reservationRepository.findForHost(host);
     }
 
     public List<Reservation> findForGuestAndHost(Host host, Guest guest) {
-        return repository.findForGuestAndHost(host, guest);
+        return reservationRepository.findForGuestAndHost(host, guest);
     }
 
-    public Result add(Reservation reservation) {
-        Result result = validateAddition(reservation);
+    public Result add(Reservation reservation) throws DataException {
+        Result result = validateAdd(reservation);
         if (!result.isSuccess()) {
             return result;
         }
 
-        result.setPayload(repository.add(reservation));
+        result.setPayload(reservationRepository.add(reservation));
         return result;
     }
 
-    public Result update(Reservation reservation) {
-        Result result = validateUpdation(reservation);
+    public Result update(Reservation reservation) throws DataException {
+        Result result = validateUpdate(reservation);
         if (!result.isSuccess()) {
             return result;
         }
 
-        result.setPayload(repository.update(reservation));
+        boolean isUpdated = reservationRepository.update(reservation);
+        if (isUpdated) {
+            result.setPayload(reservation);
+        } else {
+            result.addMessage("Reservation not found.");
+        }
+
         return result;
     }
 
-    public Result delete(Reservation reservation) {
-        Result result = validateDeletion(reservation);
+    public Result delete(Reservation reservation) throws DataException {
+        Result result = validateDelete(reservation);
         if (!result.isSuccess()) {
             return result;
         }
-        boolean isDeleted = repository.delete(reservation);
+        boolean isDeleted = reservationRepository.delete(reservation);
         if (!isDeleted) {
             result.addMessage("Reservation not found.");
         }
         return result;
     }
 
-    private Result validateAddition(Reservation reservation) {
+    private Result validateAdd(Reservation reservation) {
         Result result = new Result();
-        validateFields(reservation, result); //Guest, host, and start and end dates are required.
-        validateStartAndEnd(reservation, result); //The start date must come before the end date.
-        validateOverlap(reservation, result); //The reservation may never overlap existing reservation dates.
-        validateStartDateInFuture(reservation, result); //The start date must be in the future.
+
+        validateFields(reservation, result);
+        if (!result.isSuccess()) {
+            return result;
+        }
+
+        validateChildrenExist(reservation, result);
+        if (!result.isSuccess()) {
+            return result;
+        }
+
+        validateStartAndEnd(reservation, result);
+        if (!result.isSuccess()) {
+            return result;
+        }
+
+        validateOverlap(reservation, result);
+        if (!result.isSuccess()) {
+            return result;
+        }
+
+        validateStartDateInFuture(reservation, result);
         return result;
     }
 
-    private Result validateUpdation(Reservation reservation) {
+    private Result validateUpdate(Reservation reservation) {
         Result result = new Result();
-        validateFields(reservation, result); //Guest, host, and start and end dates are required.
-        validateStartAndEnd(reservation, result); //The start date must come before the end date.
-        validateOverlap(reservation, result); //The reservation may never overlap existing reservation dates.
+
+        validateFields(reservation, result);
+        if (!result.isSuccess()) {
+            return result;
+        }
+
+        validateChildrenExist(reservation, result);
+        if (!result.isSuccess()) {
+            return result;
+        }
+
+        validateStartAndEnd(reservation, result);
+        if (!result.isSuccess()) {
+            return result;
+        }
+
+        validateOverlap(reservation, result);
+        if (!result.isSuccess()) {
+            return result;
+        }
+
+        validateStartDateInFuture(reservation, result);
+        if (!result.isSuccess()) {
+            return result;
+        }
+
+        validateFutureReservation(reservation, result);
         return result;
     }
 
-    private Result validateDeletion(Reservation reservation) {
+    private Result validateDelete(Reservation reservation) {
         Result result = new Result();
-        validateStartDateInFuture(reservation, result); //The start date must be in the future.
+        validateFutureReservation(reservation, result);
         return result;
     }
 
     private void validateFields(Reservation reservation, Result result) {
-        if (reservation.guest == null) {
+        //Guest, host, and start and end dates are required.
+        if (reservation.getGuest() == null) {
             result.addMessage("Guest is required.");
         }
-        if (reservation.host == null) {
+        if (reservation.getHost() == null) {
             result.addMessage("Host is required.");
         }
-        if (reservation.startDate == null) {
+        if (reservation.getStartDate() == null) {
             result.addMessage("Start date is required.");
         }
-        if (reservation.endDate == null) {
+        if (reservation.getEndDate() == null) {
             result.addMessage("End date is required.");
         }
     }
 
+    private void validateChildrenExist(Reservation reservation, Result result) {
+        //The guest and host must already exist in the "database".
+        if (reservation.getHost().getEmail() == null
+                || hostRepository.findByEmail(reservation.getHost().getEmail()) == null) {
+            result.addMessage("Host does not exist.");
+        }
+
+        if (reservation.getGuest().getEmail() == null
+                || guestRepository.findByEmail(reservation.getGuest().getEmail()) == null) {
+            result.addMessage("Guest does not exist.");
+        }
+    }
+
     private void validateStartAndEnd(Reservation reservation, Result result) {
-        if (reservation.getEndDate().isBefore(reservation.getStartDate())) {
-            result.addMessage("Start date must be before end date.");
+        //The start date must come before the end date.
+        if (!reservation.getStartDate().isBefore(reservation.getEndDate())) {
+            result.addMessage("The start date must come before the end date.");
         }
     }
 
     private void validateOverlap(Reservation reservation, Result result) {
-        List<Reservation> reservations = repository.findForHost(reservation.getHost());
-        boolean isOverlap = false;
-        for (Reservation r: reservations) {
-                if ((reservation.getStartDate().isAfter(r.getStartDate()) &&
-                        reservation.getStartDate().isBefore(r.getEndDate())) ||
-                        (reservation.getEndDate().isAfter(r.getStartDate()) &&
-                                reservation.getEndDate().isBefore(r.getEndDate())) ||
-                        reservation.getStartDate().isEqual(r.getStartDate()) ||
-                        reservation.getEndDate().isEqual(r.getEndDate())
-                ) {
-                    isOverlap = true;
-                }
-                if ((r.getStartDate().isAfter(reservation.getStartDate()) &&
-                        r.getStartDate().isBefore(reservation.getEndDate())) ||
-                        (r.getEndDate().isAfter(reservation.getStartDate()) &&
-                                r.getEndDate().isBefore(reservation.getEndDate())) ||
-                        r.getStartDate().isEqual(reservation.getStartDate()) ||
-                        r.getEndDate().isEqual(reservation.getEndDate())
-                ) {
-                    isOverlap = true;
-                }
-            if (isOverlap) {
+        //The reservation may never overlap existing reservation dates.
+        List<Reservation> reservations = reservationRepository.findForHost(reservation.getHost());
+
+        for (Reservation r : reservations) {
+            /* For updating a reservation, overlap should not be checked between the
+            current reservation and the new reservation*/
+            if (r.getId() == reservation.getId()) {
+                continue;
+            }
+
+            /* for each existing reservation, both start and end dates should be either before
+            the new reservation's start date or after the new reservation's end date */
+            if (!((r.getStartDate().isBefore(reservation.getStartDate()) &&
+                    r.getEndDate().isBefore(reservation.getStartDate())) ||
+                    (r.getStartDate().isAfter(reservation.getEndDate()) &&
+                            r.getEndDate().isAfter(reservation.getEndDate()))))
+            {
                 result.addMessage("The reservation must not overlap existing reservation dates.");
+                break;
             }
         }
     }
 
     private void validateStartDateInFuture(Reservation reservation, Result result) {
-        if (reservation.getStartDate().isBefore(LocalDate.now())) {
-            result.addMessage("Reservation start date cannot be in the past.");
+        //The start date must be in the future.
+        if (!reservation.getStartDate().isAfter(LocalDate.now())) {
+            result.addMessage("The reservation start date must be in the future.");
+        }
+    }
+
+    private void validateFutureReservation(Reservation reservation, Result result) {
+        //The reservation to be edited/deleted must be in the future
+        List<Reservation> reservations = reservationRepository.findForGuestAndHost(reservation.getHost(), reservation.getGuest());
+        for (Reservation r : reservations) {
+            if (r.getId() == reservation.getId()) {
+                if (!r.getStartDate().isAfter(LocalDate.now())) {
+                    result.addMessage("The reservation must be in the future.");
+                    break;
+                }
+            }
         }
     }
 }
